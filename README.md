23f312416aca3f4be68cfec96d5e53ceb3538bad
Juan Jimenez <mshj@protonmail.com>
2023-10-10 00:29:10 -0400

    Docker setup (#3)
    * initial backend set up
    
    * Dockerization of the project
    
    * fixing merge

---
+++ README.md
@@ -0,0 +1,6 @@
+# Set Up Instructions
+
+1. create an .env file (use .env_example)
+2. place it both in the main directory and the /backend directory
+3. build the docker container `docker-compose build`
+4. run the docker container `docker-compose up`
--- backend/api/routers.go
+++ backend/api/routers.go
@@ -3,11 +3,28 @@ package api
 import (
 	"cozy-backend/api/handlers"
 	"cozy-backend/internal/db"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-//Env file for the password
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+}
+
+func getDBConfig() *DBConfig {
+	return &DBConfig{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASS"),
+		Name:     os.Getenv("DB_NAME"),
+	}
+}
 
 func SetupRoutes(r *gin.Engine) {
 
@@ -15,11 +32,9 @@ func SetupRoutes(r *gin.Engine) {
 
 	r.POST("/signup", handlers.Register(pgDB))
 	r.POST("/login", handlers.Login(pgDB))
-
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello from Gin!",
 		})
 	})
-
 }
